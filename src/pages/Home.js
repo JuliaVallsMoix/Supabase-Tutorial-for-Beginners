6c030c46ed86a061fedb5c5f155c48cb30760ca0
Júlia Valls Moix <juliavalls20@gmail.com>
2023-06-21 14:19:29 +0200

    3rd video

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,24 @@
+import { logDOM } from "@testing-library/react"
+import supabase from "../Config/supabaseClient"
+import { useEffect, useState } from "react"
+
 const Home = () => {
+
+  const [fetchError, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from('smoothies')
+        .select()
+
+        if(error) {
+          setFetchError()
+        }
+    }
+  }, [])
+
   return (
     <div className="page home">
       <h2>Home</h2>
